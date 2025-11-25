654f31e6b3cffb3f3613ad7ec5aa0c27e1bcdb02
Luis Armuto <luisarmuto3@gmail.com>
2025-11-25 09:40:56 -0500

    NumberFormatException

--- src/main/java/com/exepciones/exepciones/controlador/Operaciones.java
+++ src/main/java/com/exepciones/exepciones/controlador/Operaciones.java
@@ -2,16 +2,22 @@ package com.exepciones.exepciones.controlador;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/division")
 public class Operaciones {
     @GetMapping
-    public String divide(){
-        int valor = 20/0;
-        return "Resultado: "+ valor;
+    public String divide(@RequestParam String numero){
+        try {
+            int valor = Integer.parseInt(numero);
+            int resultado = 20 / valor;
+            return "El resultado es: " + resultado;
+        }catch (NumberFormatException ERROR){
+            return "Error: el valor ingresado " + numero + " no es un número";
+        }catch (ArithmeticException ERROR){
+            return "NO se puede dividir entre 0";
+        }
     }
-
-
 }
